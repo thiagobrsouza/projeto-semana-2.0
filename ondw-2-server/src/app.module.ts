96e9518afbed859930218a5a6650f1483c755393
AR-IT Thiago Souza <thiago.souza@arit.com.br>
2022-11-09 16:02:13 -0300

    Prisma config

--- ondw-2-server/src/app.module.ts
+++ ondw-2-server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [],
   providers: [],
 })
